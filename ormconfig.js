c65e525b2bc8c6bd77d1f056ab8bfd468e2a4de1
Rafael Grande <rafael.grande@jussi.com.br>
2021-04-15 02:25:58 -0300

    Adding configuration in ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -8,7 +8,9 @@ module.exports = {
     "cli": {
         "migrationsDir": "./src/db/migrations"
     },
-    "extra": {
-      ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      }
     }
   }
